3d1451e062e988322197ac54a3d7dff1f2559686
Emiliano <EmilianoR117@outlook.com>
2025-10-10 12:34:30 -0600

    feat: creation of the validate.js and the restaurant.schema

---
+++ express-mongo-api/src/middlewares/validate.js
@@ -0,0 +1,9 @@
+module.exports = (schema, property = "body") => (req, res, next) => {
+  const { error, value } = schema.validate(req[property], {
+    abortEarly: false,
+    stripUnknown: true
+  });
+  if (error) return res.status(400).json({ error: error.details.map(d => d.message) });
+  req[property] = value;
+  next();
+};
---
+++ express-mongo-api/src/validators/restaurant.schema.js
@@ -0,0 +1,40 @@
+const Joi = require("joi");
+
+const coordSchema = Joi.array().length(2).items(Joi.number().required());
+const addressSchema = Joi.object({
+  building: Joi.string().required(),
+  street: Joi.string().required(),
+  zipcode: Joi.string().required(),
+  coord: coordSchema.required()
+});
+
+const gradeSchema = Joi.object({
+  date: Joi.date().required(),
+  score: Joi.number().integer().min(0).max(10).required()
+});
+
+const commentSchema = Joi.object({
+  date: Joi.date().required(),
+  comment: Joi.string().required(),
+  _id: Joi.string().optional()
+});
+
+const restaurantSchema = Joi.object({
+  name: Joi.string().required(),
+  borough: Joi.string().required(),
+  cuisine: Joi.string().required(),
+  restaurant_id: Joi.string().required(),
+  address: addressSchema.required(),
+  grades: Joi.array().items(gradeSchema).default([]),
+  comments: Joi.array().items(commentSchema).default([])
+});
+
+const querySchema = Joi.object({
+  borough: Joi.string(),
+  cuisine: Joi.string(),
+  name: Joi.string(),
+  page: Joi.number().integer().min(1).default(1),
+  limit: Joi.number().integer().min(1).max(50).default(10)
+});
+
+module.exports = { restaurantSchema, querySchema };
